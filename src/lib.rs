73c109734a47e819d8aa6eb7b327a38efa681ef2
Michael Daffin <michael.daffin@switchconcepts.com>
2017-10-24 14:29:58 +0100

    Proof of concept

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,20 @@
 extern crate hyper;
 extern crate futures;
-extern crate service_fn;
 extern crate reqwest;
+extern crate tokio_service;
 
-use hyper::header::{ContentLength, ContentType};
-use hyper::server::{Http, Response};
-use futures::future;
 use std::thread;
-use service_fn::service_fn;
-use std::sync::{Mutex, Arc};
 
-static TEXT: &'static str = "Hello, World!";
+use hyper::server::{Http, Response};
+use futures::Future;
+
+mod service_fn;
+pub use service_fn::{service_fn, ServiceFn};
 
 pub struct Server {
-    close: Arc<Mutex<bool>>,
+    child: Option<thread::JoinHandle<hyper::Result<()>>>,
+    done: Option<futures::sync::oneshot::Sender<()>>,
+    local_addr: std::net::SocketAddr,
 }
 pub struct ServerBuilder {}
 
@@ -21,40 +22,38 @@ impl ServerBuilder {
     pub fn run(self) -> Server {
         let handler = || {
             Ok(service_fn(|_req| {
-                println!("get a request");
-                Ok(
-                    Response::<hyper::Body>::new()
-                        .with_header(ContentLength(TEXT.len() as u64))
-                        .with_header(ContentType::plaintext())
-                        .with_body(TEXT),
-                )
+                Ok(Response::<hyper::Body>::new().with_body("hello world"))
             }))
         };
 
-        let addr = ([127, 0, 0, 1], 3000).into();
+        let (done_tx, done_rx) = futures::sync::oneshot::channel();
+        let (addr_tx, addr_rx) = std::sync::mpsc::channel();
 
+        let child = Some(thread::spawn(move || {
+            let server = Http::new().bind(&([127, 0, 0, 1], 0).into(), handler)?;
+            addr_tx.send(server.local_addr().unwrap()).unwrap();
+            server.run_until(done_rx.map_err(|_| ()))
+        }));
 
-
-        let close = Arc::new(Mutex::new(true));
-        let close_clone = close.clone();
-
-        println!("starting thread");
-        thread::spawn(move || {
-            let server = Http::new().bind(&addr, handler).unwrap();
-            println!("starting server");
-            server.run_until(future::poll_fn(|| if *close_clone.lock().unwrap() {
-                Ok(futures::Async::NotReady)
-            } else {
-                Ok(futures::Async::Ready(()))
-            }))
-        });
-        Server { close }
+        Server {
+            child,
+            done: Some(done_tx),
+            local_addr: addr_rx.recv().unwrap(),
+        }
     }
 }
 
 impl Drop for Server {
     fn drop(&mut self) {
-        *self.close.lock().unwrap() = true;
+        println!("dropping");
+        if let Some(done) = self.done.take() {
+            done.send(()).unwrap();
+        }
+        println!("joining");
+        if let Some(child) = self.child.take() {
+            child.join().unwrap().unwrap();
+        }
+        println!("done");
     }
 }
 
@@ -63,9 +62,8 @@ impl Server {
         ServerBuilder {}
     }
 
-    pub fn url(&self) -> &str {
-        "http://127.0.0.1:80"
-        //self.inner.local_addr()
+    pub fn url(&self) -> String {
+        format!("http://{}", self.local_addr)
     }
 }
 
@@ -73,5 +71,5 @@ impl Server {
 mod tests {
 
     #[test]
-    fn simple_json() {}
+    fn test_server() {}
 }
